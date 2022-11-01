13c6dd8958dc9dc01bd7fdd1e6c97aae4055d795
Robert König <mailto.koenig.robert@gmail.com>
2022-11-01 18:51:25 +0100

    Create PrintSQLData.ps1
    Print plain SQL Data from a MS-SQL Database

---
+++ PrintSQLData.ps1
@@ -0,0 +1,33 @@
+﻿# Teamcity configuration
+Set-StrictMode -Version Latest
+
+# Error Handler
+$ErrorActionPreference = "Stop"
+
+# Imports
+Import-Module SqlServer
+
+# Scheduling
+# Daily Script
+
+# Global Variables
+$serverInstance = ""
+$dbName = ""
+$schemaName = ""
+$tableName = ""
+
+# Functions
+function printData {
+    [CmdletBinding()]
+    param ($serverInstance, $dbName, $schemaName, $tableName) 
+    
+    Write-Host "[INFO] Output der Datenbank $dbName" -ForegroundColor Green
+    $data = read-sqltabledata -serverInstance $serverInstance -databasename $dbName -schemaname $schemaName -tablename $tableName -outputas datatable  
+    
+    # using echo for string formatting
+    echo $data
+}
+
+# Main
+printData $serverInstance $dbName $schemaName $tableName
+printData "server" "dbName" "dbo" "tableName"
